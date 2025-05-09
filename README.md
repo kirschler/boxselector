d077ceddbbced1e1a8c07e6d0b52e398d6ef4704
U92003 <peter.kisch@rise.se>
2025-05-09 10:23:35 +0200

    Cleanup and add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# BoxSelector
+
+This application selects the most suitable box for packing items, based on the dimensions of the items.  
+It optimizes packaging to maximize the use of available space.  
+
+## Installation
+
+**Cloning repo**  
+
+`git clone https://github.com/kirschler/boxselector.git`
--- src/main/java/com/peterkisch/boxselector/beans/BoxSelector.java
+++ src/main/java/com/peterkisch/boxselector/beans/BoxSelector.java
@@ -78,44 +78,6 @@ public class BoxSelector implements CommandLineRunner {
         }
     }
         
-//    private boolean packageItems(Box box, List<Item> items) {
-//        List<Integer> listfreeRowSpace = new ArrayList<>(); // A list of free space for each row in the box
-//        int rowNumber = 0;
-//        
-//        for (Item item : items) {
-//            boolean itemPlaced = false;
-//
-//            // Try to place the item on existing rows
-//            for (int i = 0; i < listfreeRowSpace.size(); i++) {
-//                if (listfreeRowSpace.get(i) >= item.getWidth()) {
-//                    listfreeRowSpace.set(i, listfreeRowSpace.get(i) - item.getWidth());
-//                    logger.debug("Place item (width {}) on row {}. Space left on row: {}", item.getWidth(), i + 1, listfreeRowSpace.get(i));
-//                    itemPlaced = true;
-//                    break;
-//                }
-//            }
-//
-//            // If the item couldn't be placed try to add a new row
-//            if (!itemPlaced) {
-//                if (listfreeRowSpace.size() >= box.getHeight()) {
-//                	logger.debug("No place left for item (width {}). The height of the box is {} rows.", item.getWidth(), box.getHeight());
-//                    return false; // No more rows available in the box
-//                }
-//                if (item.getWidth() > box.getWidth()) {
-//                	logger.debug("The item (width {}) is too wide for the box (width {}).", item.getWidth(), box.getWidth());
-//                    return false; // The item is too wide for the box
-//                }
-//                listfreeRowSpace.add(box.getWidth() - item.getWidth()); // Add a new row
-//                
-//                rowNumber = listfreeRowSpace.size();
-//                logger.debug("Placing item (width {}) on a new row {}. Space left on row: {}", item.getWidth(), rowNumber, listfreeRowSpace.get(rowNumber - 1));                
-//                
-//            }
-//        }
-//        logger.debug("All items placed in the box.");
-//        return true;
-//    }
-
     private String getItemListSummary(List<Item> itemList) {
         Map<Integer, Long> itemCounts = itemList.stream()
             .collect(Collectors.groupingBy(Item::getItemNumber, TreeMap::new, Collectors.counting()));
@@ -159,6 +121,5 @@ public class BoxSelector implements CommandLineRunner {
     	}
         return itemWidthMap;
     }    
-    
     
 }
